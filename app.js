214e6f2fdcdc63586b5940fea860d97e44ebcbe1
Neamar <neamar@neamar.fr>
2013-10-26 20:01:31 +0200

    Remove useless endpoints for test

--- app.js
+++ app.js
@@ -7,15 +7,5 @@ var serverConfig = require('./lib/provider-box');
 
 var server = cluestrProvider.createServer(serverConfig);
 
-var initialSync = require('./lib/provider-box/helpers/box/initial');
-server.get('/d', function(req, res, next) {
-  initialSync('Xi6PhV1soyag4iuSyIYXsPAtwuKDoXRC', function(err, tasks) {
-  });
-
-  res.send('OK');
-  next();
-});
-
-
 // Expose the server
 module.exports = server;
